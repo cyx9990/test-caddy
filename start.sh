4cc27b68e037d20e5cdda96db8ad64e9e03504fb
cyx9990 <94532537+cyx9990@users.noreply.github.com>
2022-06-14 15:40:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # configs
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget https://raw.githubusercontent.com/cyx9990/test-caddy/main/etc/index-head.html -O /usr/share/caddy/index.html
-wget -qO- $ConfigCaddyfile | sed -e "1c :$PORT" > /etc/caddy/Caddyfile
+wget -qO- $ConfigCaddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$PWD/g" > /etc/caddy/Caddyfile
 echo "this is test3" >> /usr/share/caddy/index.html
 sed -e "s/^/<p>&/g" /etc/caddy/Caddyfile >> /usr/share/caddy/index.html
 echo "----------" >> /usr/share/caddy/index.html
