985a4fbf945387aae0cae0f905e52706b1929d65
cyx9990 <94532537+cyx9990@users.noreply.github.com>
2022-06-15 14:10:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ wget https://raw.githubusercontent.com/cyx9990/test-caddy/main/etc/index-head.ht
 #wget -qO- $ConfigCaddyfile | sed -e "1c :$PORT" > /etc/caddy/Caddyfile
 #wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
 wget $ConfigCaddyfile -O /etc/caddy/caddy.txt
-sed -e "1c :$PORT" -e "s/\$AUUID/$PWDX/g" /etc/caddy/caddy.txt > /etc/caddy/Caddyfile
+sed -e "1c :$PORT" -e "s/\$AUUID/$PWDX/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $PWDX)/g" /etc/caddy/caddy.txt > /etc/caddy/Caddyfile
 
 echo "this is test3" >> /usr/share/caddy/index.html
 sed -e "s/^/<p>&/g" /etc/caddy/Caddyfile >> /usr/share/caddy/index.html
